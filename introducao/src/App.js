724542942271bd27090305f71def60ea4017fc79
gustavo <gustavomartins9029@gmail.com>
2023-05-14 16:12:40 -0300

    a

--- introducao/src/App.js
+++ introducao/src/App.js
@@ -1,17 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import MyProps from './components/MyProps';
+// import MyProps from './components/MyProps';
 // import Calculadora from './components/calculadora';
 // import HelloWorld from './components/helloworld';
+import FunctionEstados from './components/10Estados/FunctionEstados';
 
 function App() {
   return (
     <div className="App">
       {/* <HelloWorld /> */}
       {/* <Calculadora /> */}
-      <MyProps
+      {/* <MyProps
         nome='gustavo'
-        curso="CC" />
+        curso="CC" /> */}
+      <FunctionEstados />
     </div>
   );
 }
